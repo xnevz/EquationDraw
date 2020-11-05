64ad2e3a7da9571fae6d35b53d281fad63767148
bettayebabdelkrime2055 <bettayebabdelkrime2055@gmail.com>
2020-11-05 18:26:04 +0100

    fix

---
+++ Controls/Operations/RealNumber.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace EquationDraw
+{
+    /// <summary>
+    /// Interaction logic for RealNumber.xaml
+    /// </summary>
+    [ContentProperty(nameof(Value))]
+    public partial class RealNumber : Number
+    {
+
+        #region Dependency Properties
+
+        public double Value
+        {
+            get { return (double)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(double), typeof(RealNumber), new PropertyMetadata(0d));
+
+        #endregion
+
+        public RealNumber()
+        {
+            InitializeComponent();
+        }
+    }
+}
